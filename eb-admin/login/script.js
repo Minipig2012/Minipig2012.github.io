1f1fa25f13403a448ab433165a905337900db8c7
Minipig2012 <3043662639@qq.com>
2024-07-11 11:45:22 +0800

    Add files via upload

---
+++ eb-admin/login/script.js
@@ -0,0 +1,17 @@
+function validateLogin() {  
+    const username = document.getElementById('username').value;  
+    const password = document.getElementById('password').value;  
+    const messageElement = document.getElementById('message');  
+  
+    messageElement.classList.remove('visible');  
+  
+    if (username === 'Zhu' && password === 'adminzhu123') {  
+        // 登录成功，可以重定向到另一个页面或进行其他操作  
+        alert('登录成功！即将跳转...');  
+        // window.location.href = 'home.html'; // 示例：重定向到另一个页面  
+        window.location.href = '../BlogBoard.html';
+    } else {  
+        messageElement.textContent = '用户名或密码错误！';  
+        messageElement.classList.add('visible');  
+    }  
+}
